b46845e03d2cc770e90ee2b785f87d06b02ac235
Aleksandr Klimov <tzi_rme@mail.ru>
2023-06-17 20:51:37 +0300

    script & service cts connect

---
+++ cts_connect_cap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+[ "$1" == "start" ] && cts connect -auto &     
+sleep 10    
+
+while true : 
+do
+        ss -ua | grep "IP_ADRES:PORT" &>/dev/null
+        if
+                [ $? -eq 0 ] 
+        then
+                :      
+        else
+                cts disconnect && cts connect -auto &  
+        fi
+        sleep 2s 
+done
+
+[ "$1" == "stop" ] && cts disconnect   
+sleep 3s 
